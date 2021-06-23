ea517a55c231e76f4a078b51102f257a94ff0065
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-06-23 15:28:45 -0500

    commit after part 1

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'A Cinderella Story', 'The Polar Express'];
 
    constructor() { }
 
